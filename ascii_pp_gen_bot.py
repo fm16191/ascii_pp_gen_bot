783299ec3bf8c200727881731f4820288abb7267
fm16191 <fx.mordant@gmai.com>
2020-10-14 22:22:35 +0000

    rendu bot 44*44 & embed (space don't work)

--- ascii_pp_gen_bot.py
+++ ascii_pp_gen_bot.py
@@ -1,11 +1,9 @@
 # from requests import get
 # import random
-import sys
 import os
 import discord
 from discord.ext import commands
 from datetime import datetime
-import asyncio
 from fake_useragent import UserAgent
 ua = UserAgent()
 from PIL import Image
@@ -28,14 +26,85 @@ async def download_pp(author):
     path = await makedir(author)
     print(path)
     open(f"{path}/pp.png","wb").write(reqcontent)
+    print(f"{path}/pp.png correctly downloaded")
     return path
 
 async def image_to_ascii(author):
-    # path = download_pp(author)
-    path = "2020-10-13 23:36:16.472576_391582181392384000"
-    file_path = f"{path}/pp.png"
+  path = await download_pp(author)
+  #path = "2020-10-14 13:50:38.650156_705398715409498175"
+  file_path = f"{path}/pp.png"
 
-    return True
+  img = Image.open(file_path)
+  pix = img.load()
+  (sx, sy) = img.size
+  print(f"Image size : {sx}:{sy}")
+  pasx = sx / 22
+  pasy = sy / 44
+  pasm = pasx if pasx > pasy else pasy
+  print("pasx, pasy, pasm", pasx, pasy, pasm)
+
+  matpa = []
+  l = 0
+  #pasm = int(pasm)
+  for i in range(sx):
+      if int(i % (pasm / 2)) == 0:
+          lst = []
+          l = 0
+      l = l + 1
+      for j in range(sy):
+          (r, g, b) = pix[i, j]
+          try:
+              lst[int(j / pasm)] += int(r + g + b)
+          except:
+              lst.append(int(r + g + b))
+          pass
+      if int(i % (pasm / 2)) == 0 and l != 0:
+          #print(len(lst))
+          for k in range(len(lst)):
+              lst[k] = int(lst[k] / (pasm * pasm))
+          #print(lst)
+          matpa.append(lst)
+
+  print("len matpa", len(matpa), len(matpa[0]))
+  #print(matpa)
+
+  min = matpa[0][0]
+  max = matpa[0][0]
+
+  for i in range(len(matpa)):
+      for j in range(len(matpa[0])):
+          if matpa[i][j] < min:
+              min = matpa[i][j]
+          if matpa[i][j] > max:
+              max = matpa[i][j]
+
+  print("min, max", min, max)
+
+  spectre = " .:#"
+  spectre = ".:#█"
+  spectre = " ░▒▓█"
+  spectre = "░▒▓█"
+  print("len spectre : ", len(spectre))
+  taille_intensite = max - min
+  intensite_diff = taille_intensite / (len(spectre) - 1)
+  print("intensite_diff", intensite_diff)
+
+  mascii = list()
+
+  for j in range(len(matpa[0])):
+      lst = []
+      for i in range(len(matpa)):
+          intensite_case = int((matpa[i][j] - min) / intensite_diff)
+          #print(intensite_case, spectre[intensite_case])
+          lst.append(spectre[intensite_case])
+      mascii.append(lst)
+
+  strr = str(mascii).replace("[", "").replace("], ","\n").replace(", ", "").replace("]", "").replace("\'", "")
+  #print(strr)
+  print(len(strr))
+  print("len mascii", len(mascii), len(mascii[0]))
+  return strr
+
 
 
 
@@ -55,9 +124,30 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content == "&asciipp":
+    #await message.channel.send(message)
+    #await message.channel.send(message.content)
+    #await message.channel.send(message.mentions)
+    if message.content.startswith("&asciipp"):
+      #if len(message.content.split(" ")) == 2:
+      #  id = message.content.split(" ")[1]
+      #  if id.isnumeric():
+      #    author = client.get_user(id)
+      if len(message.mentions) > 0:
+        author = message.mentions[0]
+      else:
         author = message.author
-        await image_to_ascii(author)
+      strr = await image_to_ascii(author)
+      #await message.channel.send(strr)
+
+      embed = discord.Embed(title = f"Ascii pp for {author}", description = strr, color=0x29c87e)
+      embed.timestamp = datetime.utcnow()
+      embed.set_footer(text="footer : En dev", icon_url=client.get_user(391582181392384000).avatar_url)
+      await message.channel.send(embed = embed)
+      return
+
+    if message.content == "&invite" and message.author.id == 391582181392384000:
+      await message.channel.send("https://discord.com/api/oauth2/authorize?client_id=765714860221661204&permissions=8&scope=bot")
+
 
 # bot.run(TOKEN)
 client.run(TOKEN)
